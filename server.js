f97c8974c4185bfd76f5f80fdc1d1317e9681029
Hadixp <hadi.b.2002@gmail.com>
2024-12-30 20:40:11 +0330

    ad

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // this server respose statuse code 500 using express on github
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+
 app.all('*', (req, res) => {
-    res.status(500).send('serverError');
+    const respon = axios.get('https://hadi-xp.github.io/serverError/');
+    res.status(500).send(`${respon}`);
 });
 
 app.listen(PORT, () => {
