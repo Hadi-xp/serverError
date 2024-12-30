3283395e3b082cc28457834060f19c61dde14a70
Hadixp <hadi.b.2002@gmail.com>
2024-12-30 20:25:32 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.status(500).send('serverError');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
