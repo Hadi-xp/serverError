0415cac44f43ffd77605e3ac14b5de23b9d37d41
Hadixp <hadi.b.2002@gmail.com>
2024-12-30 22:11:23 +0330

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
-app.get('/', (req, res) => {
+app.get('/api/error', (req, res) => {
     res.status(500).send('Internal Server Error');
 });
 
